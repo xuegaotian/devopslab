f45fa85ce788b46bb903c30974895057a0dc7bac
tian <50902706+xuegaotian@users.noreply.github.com>
2019-12-07 14:10:46 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=xuegaotian
 
 export MYCLUSTER=tektonknativebeijing35
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing35/kube-config-tok02-tektonknativebeijing35.yml
 
 export NAMESPACE=xuegaotian
 出口 EMAIL =gao_tian007@163.com
-export INGRESS=
+export INGRESS=tektonknativebeijing35-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
